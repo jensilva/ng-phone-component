9cae21a2740a68e87d05c80244fbd3883ef8fd32
Jennfer Silva <jenny.ms@outlook.com.br>
2019-08-02 15:56:14 +0100

    feat: get keys on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Phone';
   buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'];
-  buttonCall = true;
+  onCall = true;
+  buttonsPressed = '';
+
+  getKeys(key: string): string {
+    this.buttonsPressed += key;
+    return this.buttonsPressed;
+  }
 }
