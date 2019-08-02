ec72c24375eec05eaeb6865e1e946a8ad86c256a
Jennfer Silva <jenny.ms@outlook.com.br>
2019-08-02 14:04:28 +0100

    buttons UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Phone';
+  buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'];
+  buttonCall = true;
 }
