608dc9a46ea4bd703e5abfbca66579bfcd0f6044
Jennfer Silva <jenny.ms@outlook.com.br>
2019-08-05 16:21:02 +0100

    fix: layout, feat: call button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Phone';
   buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'];
-  onCall = true;
+  onCall = false;
   buttonsPressed = '';
 
   getKeys(key: string): string {
     this.buttonsPressed += key;
     return this.buttonsPressed;
+  }
+
+  call(): void {
+    this.onCall = !this.onCall;
+    this.buttonsPressed = '';
+    console.log(this.onCall);
+  }
+
+  get buttonMessage(): string {
+    if (this.onCall) {
+      return 'Dismiss Call';
+    } else {
+      return 'Call';
+    }
   }
 }
